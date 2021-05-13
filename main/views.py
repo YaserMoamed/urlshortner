40b8540f58fe3c50194af610b9fa4abf03d17930
YaserMoamed <yasermoamd@gmail.com>
2021-05-13 02:12:31 +0200

    updated error side of usrl shortner

--- main/views.py
+++ main/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
+from .models import short_urls
+from .forms import UrlForm
+from .shortner import shortner
+
 
 # Create your views here.
-def Make(request):
-    return render(request, 'home.html')
+def home(token):
+    long_url = short_urls.objects.filter(short_url=token)[0]
+    return redirect(long_url.long_url, )
+
+
+def make(request):
+    form = UrlForm(request.POST)
+    a = ""
+    if request.method == 'POST':
+        if form.is_valid():
+            new_url = form.save(commit=False)
+            a = shortner().issue_token()
+            new_url.short_url = a
+            print(a)
+            new_url.save()
+        else:
+            form = UrlForm()
+            a = "Invalid Url"
+    return render(request, 'home.html', {'form': form, 'a': a})
